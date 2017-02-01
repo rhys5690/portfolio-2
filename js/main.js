b2de263cbe4cbf82ee36b0d6ebb33fde91055e09
rhys5690 <rhys5690@gmail.com>
2017-02-02 09:41:00 +1100

    added scroll on click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
 console.log($);
+
+$(document).ready(function() {
+
+    $(".about-me-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".about-me").offset().top
+        }, 'slow');
+    });
+
+    $(".education-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".education").offset().top
+        }, 'slow');
+    });
+
+    $(".skills-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".skills").offset().top
+        }, 'slow');
+    });
+
+    $(".work-experience-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".work-experience").offset().top
+        }, 'slow');
+    });
+
+    $(".projects-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".projects").offset().top
+        }, 'slow');
+    });
+
+    $(".contact-button").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".contact").offset().top
+        }, 'slow');
+    });
+
+});
