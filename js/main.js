19acbda790cd8b37c442feefc892c976a2a38625
rhys5690 <rhys5690@gmail.com>
2017-02-03 16:51:14 +1100

    added type.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function() {
 
     new WOW().init();
 
+    var timerId = setTimeout(function() {
+        $(".my-butt").removeClass("none");
+        $(".my-butt").addClass("animated flipInY hvr-grow-rotate");
+    }, 15000);
+
+    $(function() {
+        $(".element-test").typed({
+            strings: [
+                "<h5> Hi, I'm Rhys </h5>", "<h5>And I have an unusual story to share with you...</h5>", "<h5>It's about a lawyer who left the legal profession to pursue his dream of becoming a front-end developer </h5>", "<h5>Click the button below to find out more.</h5>", "",
+            ],
+            typeSpeed: 0
+        });
+    });
+
+
     $(".about-me-button").click(function() {
         $('html, body').animate({
             scrollTop: $(".about-me").offset().top
